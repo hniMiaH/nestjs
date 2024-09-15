9b62f972112b4dc452e0d244f935462345312aec
hniMiaH <nghaiminh1004@gmail.com>
2024-09-15 20:53:13 +0700

    add_username

---
+++ db/migrations/1726407707554-add_username.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddUsername1726407707554 implements MigrationInterface {
+    name = 'AddUsername1726407707554'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" ADD "username" character varying`);
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username")`);
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email")`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb"`);
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "username"`);
+    }
+
+}
