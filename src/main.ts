d18737248c3f47d34bfc7a40e4aa6447a9d7b38a
hniMiaH <nghaiminh1004@gmail.com>
2024-12-26 12:04:06 +0700

    aa

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     transform: true,
   }));
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:5000',
+    origin: ['https://talk-town.vercel.app', 'http://localhost:5000'],
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
